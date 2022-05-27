d19157eedb3714923ea8cf3fbd1032a77a95a807
ChoiRang <jkchldyd@gmail.com>
2022-05-27 19:57:12 +0900

    modify code

--- pythonProject/programmers/lv2/jumping_and_teleport.py
+++ pythonProject/programmers/lv2/jumping_and_teleport.py
@@ -1,17 +1,18 @@
-def solution(n):
-	jump_count = 0
-	while n != 0:
-		if n % 2 == 0:
-			n /= 2
-		else:
-			n -= 1
+def solution(n):		# 시간초과.. ?
+	jump_count = 1
+	while n > 2:
+		if n % 2 == 1:
 			jump_count += 1
+			n -= 1
+		n /= 2
 	return jump_count
+
+
+def solution2(n):		# note
+	return bin(n)[2:].count('1')
 
 
 if __name__ == '__main__':
 	print(solution(5))
 	print(solution(6))
 	print(solution(5000))
-
-
