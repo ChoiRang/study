5c6957915d8866dd181ce64a1df53559315fdbee
ChoiRang <jkchldyd@gmail.com>
2022-09-26 18:19:55 +0900

    programmers lv1
    없는 숫자 더하기

---
+++ pythonProject/programmers/lv1/sum_missing_number.py
@@ -0,0 +1,9 @@
+def solution(numbers):
+	answer = 45
+	answer -= sum(numbers)
+	return answer
+
+
+if __name__ == '__main__':
+	print(solution([1, 2, 3, 4, 6, 7, 8, 0]))		# 14
+	print(solution([5, 8, 4, 0, 6, 7, 9]))		# 6
