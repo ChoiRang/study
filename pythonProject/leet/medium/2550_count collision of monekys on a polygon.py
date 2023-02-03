4645ae47ddb34dafa1033d7e6a0d6da7dfcff119
ChoiRang <jkchldyd@gmail.com>
2023-02-03 19:10:22 +0900

    leet code add comment

--- pythonProject/leet/medium/2550_count collision of monekys on a polygon.py
+++ pythonProject/leet/medium/2550_count collision of monekys on a polygon.py
@@ -1,7 +1,7 @@
 class Solution:
     def monkeyMove(self, n: int) -> int:
         max_int = 10 ** 9 + 7
-        return (pow(2 , n, max_int)-2) % max_int
+        return (pow(2, n, max_int)-2) % max_int
 
 
 """
@@ -17,4 +17,8 @@ Testcase
 100
 996194169
 500000003
+-----
+각 정점의 원숭이는 좌, 우 이동만 가능(2^n)
+원숭이 -> 충돌 + 충돌X = 전체 이동갯수, 충돌하지 않는 경우는 전부 좌 또는 우측으로 이동했을 경우 -> 2^n - 2
+pow(a, b, c) == a ^ b % c -> pow() 속도가 더 빠름
 """
