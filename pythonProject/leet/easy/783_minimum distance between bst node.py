556d3a80fbbce6f009eb8057734a5c630cabef9f
ChoiRang <jkchldyd@gmail.com>
2023-02-07 17:43:48 +0900

    leet code solution 783 BST

---
+++ pythonProject/leet/easy/783_minimum distance between bst node.py
@@ -0,0 +1,28 @@
+from typing import *
+
+
+# Definition for a binary tree node.
+class TreeNode:
+	def __init__(self, val=0, left=None, right=None):
+		self.val = val
+		self.left = left
+		self.right = right
+
+
+class Solution:
+	def minDiffInBST(self, root: Optional[TreeNode]) -> int:
+		result = 10 ** 5 + 1
+		prev = -10 ** 5 + 1			# 결과값 작아지는거 방지
+		stack = []
+		node = root
+		while stack or node:
+			while node:
+				stack.append(node)
+				node = node.left
+
+			node = stack.pop()
+			result = min(result, node.val - prev)
+			prev = node.val
+			node = node.right
+
+		return result
